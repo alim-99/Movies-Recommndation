baaf58d146f2150533f2d3ea1469a0a63ac5a101
alim-99 <alim16097side@gmail.com>
2025-06-10 17:22:10 +0300

    updated the name of next config

---
+++ next.config.js
@@ -0,0 +1,21 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/w342/**',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
+};
+
+export default nextConfig;
